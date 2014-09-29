097e895d4759d6c71253211fb706d5a3baf46ecd
John Roach <johnroach1985@gmail.com>
2014-09-29 22:27:37 +0300

    Added Voltage values for Pressure and Temperature sensors.  Small note Pressure Sensor has 0.51, Temperture sensor has 0.09.

--- oghma_data_browser.js
+++ oghma_data_browser.js
@@ -67,27 +67,75 @@ io.set('browser client etag', true);  // apply etag caching logic based on versi
 
 console.log('Server running on: http://' + getIPAddress() + ':8090');
 
+
+
 io.sockets.on('connection', function (socket) {
     socket.on('readData', function (data) {
         console.log(data);
         if (data == 'true'){
-            //this is where sesor reads happen... need to make this a little more
-            //modular... It all depends on what the team brings in...
+
             var temp = 0;
-            console.log(b.analogRead('P9_33') + '');
-            temp = parseFloat(b.analogRead('P9_33'));
-            var pressure = 0;
-            var analogVoltage = temp*1.8; // ADC Value converted to voltage
-            console.log('' + analogVoltage);
-            pressure = (analogVoltage*3.2 -2.44)*1000/19.52;
-            socket.emit('1stsensorvalue', pressure);
-            socket.broadcast.emit('1stsensorvalue', pressure);
+
+
+            //here we check which sensors are connected
+            //on port 1
+            if( (parseFloat(b.analogRead('P9_39')).toFixed(2)==0.51) )
+            {
+                console.log("Pressure sensor connected on port 1");
+            }else if((parseFloat(b.analogRead('P9_39')).toFixed(2)==0.09))
+            {
+                console.log("Temp sensor connected on port 1");
+            }
+
+            //here we check which sensors are connected
+            //on port 2
+            if( (parseFloat(b.analogRead('P9_37')).toFixed(2)==0.51) )
+            {
+                console.log("Pressure sensor connected on port 2");
+            }else if((parseFloat(b.analogRead('P9_37')).toFixed(2)==0.09))
+            {
+                console.log("Temp sensor connected on port 2");
+            }
+
+            //here we check which sensors are connected
+            //on port 3
+            if( (parseFloat(b.analogRead('P9_35')).toFixed(2)==0.51) )
+            {
+                console.log("Pressure sensor connected on port 3");
+            }else if((parseFloat(b.analogRead('P9_35')).toFixed(2)==0.09))
+            {
+                console.log("Temp sensor connected on port 3");
+            }
+
+
+//            console.log(b.analogRead('P9_33') + '');
+//            temp = parseFloat(b.analogRead('P9_33'));
+//            var pressure = 0;
+//            var analogVoltage = temp*1.8; // ADC Value converted to voltage
+//            console.log('' + analogVoltage);
+//            pressure = (analogVoltage*3.2 -2.44)*1000/19.52;
+//            socket.emit('1stsensorvalue', pressure);
+//            socket.broadcast.emit('1stsensorvalue', pressure);
         }
     });
 });
 
 
+function printPressure(x) {
 
+    var pressure = 0;
+
+    var analogVoltage = x.value*1.8; // ADC Value converted to voltage
+
+    console.log('Analog Voltage = ' + analogVoltage);
+
+    pressure = (analogVoltage*2.7 -2.4)*1000/19.2;
+
+    console.log("Pressure = " +
+
+        parseFloat(pressure).toFixed(3) + " mBar.");
+
+}
 
 
 // Get server IP address on LAN
